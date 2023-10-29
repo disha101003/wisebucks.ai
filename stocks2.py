362bc662a73ae7e64d12941a8c92684189d27b51
anuj-thakkar <anujtthakkar@gmail.com>
2023-10-29 19:37:22 -0400

    fix to database

--- stocks2.py
+++ stocks2.py
@@ -22,7 +22,6 @@ def create_db():
     stocks = db.Table('stocks', metadata,
         db.Column('id', db.Integer(), primary_key=True), # Primary key
         db.Column('symbol', db.String(255), nullable=False),  # Symbol is marked as non-nullable
-        db.Column('sector', db.String(255), nullable=True),
         db.Column('date', db.Date(), nullable=False),  
         db.Column('close', db.Float(), nullable=True),
         db.Column('volume', db.Integer(), nullable=True),
@@ -39,10 +38,6 @@ def create_db():
 if __name__ == "__main__":
     import os
 
-    # Delete the SQLite database file if it exists
-    if os.path.exists('atradebot.db'):
-        os.remove('atradebot.db')
-
     # Create a new database
     engine, connection, stocks = create_db()
 
@@ -56,20 +51,32 @@ if __name__ == "__main__":
     # store each DF in Stocks table in the database
     today = datetime.date.today().strftime('%Y-%m-%d')
 
-    for symbol in symbols:
-        # get stock info
-        try:
-            # Get historical data using yf.download()
-            stock_info = yf.download(symbol, start='2020-01-01', end=today) # API call to create DataFrame
-            stock_info['symbol'] = symbol # Add a column for the symbol 
-            stock_info['date'] = stock_info.index.strftime('%Y-%m-%d') # Add a column for the date
-            stock_info = stock_info[['symbol', 'date', 'Close', 'Volume', 'Open', 'High', 'Low']] # Reorder the columns
-            stock_info.columns = ['symbol', 'date', 'close', 'volume', 'open', 'high', 'low'] # Rename the columns
 
-            # Insert the aggregated data into the database
-            stock_info.to_sql('stocks', con=engine, if_exists='append', index=False)
-            print(f"Added {symbol} to the database")
-        except Exception as e:
-            print(f"Could not add {symbol} to the database")
-            print(e)
-        time.sleep(1)
+    # Define the start date for new data
+    start_date = '2020-01-01'  # Adjust this date as needed
+
+    for symbol in symbols:
+        # Check if data for this symbol and start_date already exists in the database
+        query = f"SELECT COUNT(*) FROM stocks WHERE symbol = '{symbol}' AND date >= '{start_date}'"
+        result = connection.execute(text(query)).fetchone()
+
+
+        if result[0] > 0:
+            print(f"Data for {symbol} from {start_date} onwards already exists. Skipping.")
+        else:
+        # get stock info
+            try:
+                # Get historical data using yf.download()
+                stock_info = yf.download(symbol, start=start_date)  # API call to create DataFrame
+                stock_info['symbol'] = symbol # Add a column for the symbol
+                stock_info['date'] = stock_info.index.strftime('%Y-%m-%d')  # Add a column for the date
+                stock_info = stock_info[['symbol', 'date', 'Close', 'Volume', 'Open', 'High', 'Low']]  # Reorder the columns
+                stock_info.columns = ['symbol', 'date', 'close', 'volume', 'open', 'high', 'low']  # Rename the columns
+
+                # Insert the aggregated data into the database
+                stock_info.to_sql('stocks', con=engine, if_exists='append', index=False)
+                print(f"Added {symbol} to the database")
+            except Exception as e:
+                print(f"Could not add {symbol} to the database")
+                print(e)
+            time.sleep(1)
