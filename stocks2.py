ee73f18d15acbf5ccca8422e8dd89d2aefd25eee
anuj-thakkar <anujtthakkar@gmail.com>
2023-11-02 17:00:58 -0400

    updating data efficiently to database

--- stocks2.py
+++ stocks2.py
@@ -4,16 +4,11 @@ import sqlalchemy as db
 import yfinance as yf
 from sqlalchemy import text
 import time
-from ib_insync import *
-# importing module
-import time
-import openai
 import pandas as pd
 import datetime
 
-
 def create_db():
-    # create database:
+    # Create database:
     engine = db.create_engine('sqlite:///atradebot.db', echo=True)
     connection = engine.connect()
     metadata = db.MetaData()
@@ -30,53 +25,59 @@ def create_db():
         db.Column('low', db.Float(), nullable=True),
     )
 
-
-    # create table in database:
+    # Create table in the database:
     metadata.create_all(engine)
     return engine, connection, stocks
 
-if __name__ == "__main__":
-    import os
-
-    # Create a new database
-    engine, connection, stocks = create_db()
-
-    connection.execute(text("PRAGMA journal_mode=WAL"))
-
-    # get list of stocks:
+def update_db(engine, connection):
+    # Get the list of stock symbols from the CSV
     stock_df = pd.read_csv('sp-500-index-10-29-2023.csv')
     symbols = stock_df['Symbol'].tolist()
-
-    # use YFinance to create a dataframe of all the stocks in the S&P 500
-    # store each DF in Stocks table in the database
-    today = datetime.date.today().strftime('%Y-%m-%d')
-
-
-    # Define the start date for new data
-    start_date = '2020-01-01'  # Adjust this date as needed
-
+    
+    # Specify today's date
+    today = datetime.date.today()
+    
     for symbol in symbols:
-        # Check if data for this symbol and start_date already exists in the database
-        query = f"SELECT COUNT(*) FROM stocks WHERE symbol = '{symbol}' AND date >= '{start_date}'"
+        # Find the most recent date stored in the database for this symbol
+        query = f"SELECT MAX(date) FROM stocks WHERE symbol = '{symbol}'"
         result = connection.execute(text(query)).fetchone()
-
-
-        if result[0] > 0:
-            print(f"Data for {symbol} from {start_date} onwards already exists. Skipping.")
+        most_recent_date = result[0]
+        
+        if most_recent_date is None:
+            # If no data exists for this symbol, start from a fixed date (e.g., '2020-01-01')
+            start_date = datetime.date(2020, 1, 1)
         else:
-        # get stock info
+            # If data exists, start from the day after the most recent date
+            most_recent_date = datetime.datetime.strptime(most_recent_date, '%Y-%m-%d').date()
+            start_date = most_recent_date + datetime.timedelta(days=1)
+        
+        if start_date <= today:
             try:
-                # Get historical data using yf.download()
-                stock_info = yf.download(symbol, start=start_date)  # API call to create DataFrame
-                stock_info['symbol'] = symbol # Add a column for the symbol
-                stock_info['date'] = stock_info.index.strftime('%Y-%m-%d')  # Add a column for the date
-                stock_info = stock_info[['symbol', 'date', 'Close', 'Volume', 'Open', 'High', 'Low']]  # Reorder the columns
-                stock_info.columns = ['symbol', 'date', 'close', 'volume', 'open', 'high', 'low']  # Rename the columns
-
-                # Insert the aggregated data into the database
-                stock_info.to_sql('stocks', con=engine, if_exists='append', index=False) 
-                print(f"Added {symbol} to the database")
+                stock_info = yf.download(symbol, start=start_date, end=today)  # API call to create DataFrame
+                if not stock_info.empty:
+                    stock_info['symbol'] = symbol
+                    stock_info['date'] = stock_info.index.strftime('%Y-%m-%d')
+                    stock_info = stock_info[['symbol', 'date', 'Close', 'Volume', 'Open', 'High', 'Low']]
+                    stock_info.columns = ['symbol', 'date', 'close', 'volume', 'open', 'high', 'low']
+                    
+                    # Update the database with the new data
+                    stock_info.to_sql('stocks', con=engine, if_exists='append', index=False, index_label='date')
+                    
+                    print(f"Updated {symbol} data from {start_date} to {today}")
+                else:
+                    print(f"No data available for {symbol} from {start_date} to {today}")
             except Exception as e:
-                print(f"Could not add {symbol} to twhe database")
+                print(f"Could not update {symbol} data from {start_date} to {today}")
                 print(e)
             time.sleep(1)
+        else:
+            print(f"Data for {symbol} is already up to date. Skipping.")
+
+if __name__ == "__main__":
+    # Check if the database exists and create it if not
+    if not os.path.exists('atradebot.db'):
+        create_db()
+    else:
+        engine, connection, _ = create_db()
+    
+    update_db(engine, connection)
