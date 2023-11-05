b69391de46f7cfeb44173fcc3689b88796360560
shah672 <109067879+shah672@users.noreply.github.com>
2023-11-05 14:57:42 -0500

    Update user.js

--- paperTrading/user.js
+++ paperTrading/user.js
@@ -1,14 +1,38 @@
-from flask_sqlalchemy import SQLAlchemy
+-- Users table
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    available_funds DECIMAL(15, 2) NOT NULL
+);
 
-db = SQLAlchemy()
+//replace stocks table with new web scraping code
+-- Stocks table (  
+CREATE TABLE stocks (
+    stock_id INT PRIMARY KEY AUTO_INCREMENT,
+    symbol VARCHAR(10) NOT NULL,
+    current_price DECIMAL(15, 2) NOT NULL
+);
 
-class User(db.Model):
-    userId = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fullName = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    mobile = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    availableFunds = db.Column(db.Numeric(10, 2), default=100000.00)
+-- User stocks table to keep track of user's stocks
+CREATE TABLE user_stocks (
+    user_id INT,
+    stock_id INT,
+    quantity INT NOT NULL,
+    average_price DECIMAL(15, 2) NOT NULL,
+    PRIMARY KEY (user_id, stock_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (stock_id) REFERENCES stocks(stock_id)
+);
 
-    def __repr__(self):
-        return '<User %r>' % self.fullName
+-- Transactions table
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    stock_id INT NOT NULL,
+    quantity INT NOT NULL, -- Positive for buy, negative for sell
+    price_at_transaction DECIMAL(15, 2) NOT NULL,
+    transaction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (stock_id) REFERENCES stocks(stock_id)
+);
+
