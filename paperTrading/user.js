6387b6c30e532cf2420a7abdc445d9d98c40c9bd
shah672 <109067879+shah672@users.noreply.github.com>
2023-11-05 12:09:11 -0500

    Update user.js

--- paperTrading/user.js
+++ paperTrading/user.js
@@ -1,51 +1,14 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:'); // Example for SQLite
+from flask_sqlalchemy import SQLAlchemy
 
-class User extends Model {}
+db = SQLAlchemy()
 
-User.init({
-  // Assuming you have an auto-incrementing primary key
-  userId: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
-  },
-  fullName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true
-    }
-  },
-  mobile: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  availableFunds: {
-    type: DataTypes.DECIMAL(10, 2), // Adjust precision and scale according to requirements
-    allowNull: false,
-    defaultValue: 100000.00
-  }
-}, {
-  sequelize,
-  modelName: 'User',
-  timestamps: true, // Enable timestamps
-  createdAt: 'created_at', // Rename field if you want to match MongoDB's `timestamps`
-  updatedAt: 'updated_at'
-});
+class User(db.Model):
+    userId = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fullName = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    mobile = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    availableFunds = db.Column(db.Numeric(10, 2), default=100000.00)
 
-// Syncs with the database, creating the table if it doesn't exist
-sequelize.sync();
-
-module.exports = User;
+    def __repr__(self):
+        return '<User %r>' % self.fullName
